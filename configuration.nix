b91821d67d8d77d782b654ca538230dde4db5a03
vv <felix.buchholz@pm.me>
2025-10-13 21:44:59 +0200

    enabled virtual box

--- configuration.nix
+++ configuration.nix
@@ -143,7 +143,7 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
 
-
+  virtualisation.virtualbox.host.enable = true;
 
 
 
