49250df1908c229bf171d282ab70d6de238c7a15
vv <felix.buchholz@pm.me>
2025-10-15 20:00:20 +0200

    added module vm

---
+++ modules/vm.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, ... }:
+
+{
+
+  # enable dconf (system management tool)
+  programs.dconf.enable = true;
+
+  # add user to libvirtd group
+  users.users.vv.extraGroups = [ "libvirtd" ];
+
+  # packages
+  environment.systemPackages = with pkgs; [
+    virt-manager
+    virt-viewer
+    spice spice-gtk
+    spice-protocol
+    win-virtio
+    win-spice
+    gnome.adwaita-icon-theme
+  ];
+
+  # manage virtualization services
+  virtualization = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        swtpm.enable = true;
+	ovmf.enable = true;
+	ovmf.packages = [ pkgs.OVMFFull.fd ];
+      };
+    };
+    spiceUSBRedirection.enable = true;
+  };
+  services.spice-vdagentd.enable = true;
+
+};
