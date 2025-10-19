0b25fcfdf9f9502e8b27c8d721a181b9a125953b
vv <vv@mail.mail>
2025-10-19 13:42:03 +0200

    Update gaming.nix

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -8,12 +8,12 @@
 
   config = lib.mkIf config.gaming.enable {
 
-      programs.steam.enable = true;
+    programs.steam.enable = true;   # steam
 
     # packages
     environment.systemPackages = with pkgs;
     [
-
+      mangohud  # overlay for fps, temps etc.
     ];
 
 
