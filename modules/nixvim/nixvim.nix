79fb0806c46cc22930e904649f3c85680acc7e30
vv <felix.buchholz@pm.me>
2025-12-12 19:08:24 +0100

    nixvim

--- modules/nixvim/nixvim.nix
+++ modules/nixvim/nixvim.nix
@@ -1,8 +1,20 @@
 { config, pkgs, ... }:
 
 {
+let
+  # Make relative path absolute relative to this file
+  nixvimFlake = builtins.path ./;
+in
+{
   environment.systemPackages = [
-    # Replace <flake-path> with the path to your flake or flake URL
-    (import "./" {}).packages.${pkgs.system}.default
+    (builtins.getFlake nixvimFlake).packages.${pkgs.system}.default
   ];
 }
+
+
+#  environment.systemPackages = [
+#    # Replace <flake-path> with the path to your flake or flake URL
+#    (import "./" {}).packages.${pkgs.system}.default
+#  ];
+#}
+
