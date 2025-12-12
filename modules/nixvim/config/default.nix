e7336e2e5ac1cbe0d40bbb0ae7e3f5705b33363f
vv <felix.buchholz@pm.me>
2025-12-12 22:38:32 +0100

    nixvim

--- modules/nixvim/config/default.nix
+++ modules/nixvim/config/default.nix
@@ -1,17 +1,92 @@
 { inputs, ... }:
 {
-  # Import all your configuration modules here
+
   imports = [
     ./bufferline.nix 
-    ];
+  ];
+    
+  plugins = {
+    lualine.enable = true;
+    treesitter.enable = true;
 
-  
-    vimAlias = true;
-    viAlias = true;
-
-    opts = {
-      number = true;
-
+    # file browser
+    oil = {
+      enable = true;
+      settings = {
+        view_options.show_hidden = true;
+      };
     };
+
+    #language servers
+    lsp = {
+      enable = true;
+      servers = {
+        tsserver.enable = true;  # javascript / typescript
+        lua-ls.enable = true;  # lua
+        rust-analyzer.enable = true;  # rust
+      };
+    };
+
+    # completion
+    cmp = {
+      enable = true;
+      autoEnableSources = true;
+      settings = {
+        sources = [
+          {name = "nvim_lsp";}
+          {name = "path";}
+          {name = "buffer";}
+        ];
+        mapping = {
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-e>" = "cmp.mapping.close()";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+        };
+      };
+    };
+
+    # tree file browser
+    nvim-tree = {
+      enable = true;
+      autoLoad = true;
+      openOnSetup = true;
+      openOnSetupFile = true;
+    };
+  };  
+
   
+  colorschemes.gruvbox.enable = true;
+  
+  opts = {
+    number = true; # display line numbers
+
+    # tab indenting
+    tabstop = 2;  # number of columns between two tab stops
+    shiftwidth = 0;  # how tabs are displayed 0 means use tabstop value
+    expandtab = true;  # uses spaces instead of tab-indents
+    smartindent = true;  # tries to indent automatically
+
+    # search
+    ignorecase = true;
+    smartcase = true;  # dont ignore when i search for upper case
+
+    # fold
+    foldmethod = "indent";
+    foldlevel = 99;  # doesnt fold by default
+
+    # visuals
+    termguicolors = true;  # enables 24bit color
+
+    # other
+    cursorline = true;  # highlights line cursor is in
+    scrolloff = 8;  # when less lines are left below or above the cursor it scrolls
+    wrap = false;  # wrapping lines
+    sidescrolloff = 8;  # like scrolloff
+      
+
+  };
 }
--- modules/nixvim/flake.nix
+++ modules/nixvim/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very basic flake";
+  description = "neovim packaged using nixvim";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
